f3f7be79f5e1f925040f0a93d7f8b925733b222f
Marc César Forte <99797041+marc-cesar@users.noreply.github.com>
2023-07-30 13:24:10 +0200

    Setup of the backend - Project Creation

---
+++ Backend_Layer/TFG-Backend/src/main/java/com/backend/tfgbackend/Controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.backend.tfgbackend.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/sayHello")
+    public String sayHelloWorld(){
+        return "Hello World";
+    }
+}
